b21dd91eb076ce5a1d7b8675fdceb5bef6a8f09e
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-05-25 16:14:34 +0200

    fixed crosshair and tail

--- main.lua
+++ main.lua
@@ -10,31 +10,35 @@ function love.load()
              , character = love.graphics.newImage("assets/character.png")
              }
 
-    Cursor = {x = love.mouse.getX(), y = love.mouse.getY(), image = love.graphics.newImage("assets/crosshair.png")}
+    Cursor = { x = love.mouse.getX()
+             , y = love.mouse.getY()
+             , tail = {image = love.graphics.newImage("assets/cursortail.png"), angle = 0}
+             }
 
 end
 
 function love.update(dt)
     Cursor.x = love.mouse.getX()
     Cursor.y = love.mouse.getY()
+    MouseTail()
 end
 
 function love.draw()
     love.graphics.setBackgroundColor(1,1,1)
-    love.graphics.rectangle("line", (W/2)-0.4*W, (H/2)-0.4*H, 0.8*W, 0.8*H)
+    love.graphics.draw(Cursor.tail.image, Player.position.x, Player.position.y, Cursor.tail.angle)
     love.graphics.draw(Player.character, Player.position.x, Player.position.y)
-    love.graphics.line(Player.position.x, Player.position.y, Cursor.x, Cursor.y)
 end
 
+-- Move gets the distance it should move and at what angle, from 0 to 2 pi
 function love.keypressed(key)
     if key == "right" then
         Move(10, 0)
     elseif key == "down" then
-        Move(10, 90)
+        Move(10, 0.5*math.pi)
     elseif key == "left" then
-        Move(10, 180)
+        Move(10, 1*math.pi)
     elseif key == "up" then
-        Move(10, 270)
+        Move(10, 1.5*math.pi)
     end
 end
 
@@ -43,4 +47,10 @@ function Move(distance, alpha)
     local y = distance*math.sin(alpha)
     Player.position.x = Player.position.x + x
     Player.position.y = Player.position.y + y
+end
+
+function MouseTail()
+    local px, py = Player.position.x , Player.position.y
+    local dx, dy = Cursor.x - px, Cursor.y - py
+    Cursor.tail.angle = math.atan2(dy, dx)
 end
