5cc083614f1aa84514312ad359e2af026283134f
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-05-25 08:51:11 +0200

    added player

--- main.lua
+++ main.lua
@@ -2,12 +2,12 @@ function love.load()
     DTotal = 0
     love.window.setMode(1920, 1080, {fullscreen=true, resizable=true, vsync=false, minwidth=400, minheight=300})
     W, H = love.graphics.getDimensions()
-    love.mouse.setVisible(false)
+    love.mouse.setVisible(true)
     Player = { position = {x = 0, y = 0}
              , hp       = 100
              , inventory = {}
              , hand = nil
-             , character = love.graphics.rectangle("line", 10, 10 ,10 ,10)
+             , character = love.graphics.newImage("assets/character.png")
              }
 
     Cursor = {x = love.mouse.getX(), y = love.mouse.getY(), image = love.graphics.newImage("assets/crosshair.png")}
@@ -22,7 +22,8 @@ end
 function love.draw()
     love.graphics.setBackgroundColor(1,1,1)
     love.graphics.rectangle("line", (W/2)-0.4*W, (H/2)-0.4*H, 0.8*W, 0.8*H)
-    love.graphics.draw(Cursor.image, Cursor.x-25, Cursor.y-25)
+    love.graphics.draw(Player.character, Player.position.x, Player.position.y)
+    love.graphics.line(Player.position.x, Player.position.y, Cursor.x, Cursor.y)
 end
 
 function love.keypressed(key)
