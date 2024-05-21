3278906c335519e310c5a5abec5ecb8a93755549
Edvin Källström <edvin.kallstrom@protonmail.com>
2024-05-21 12:49:53 +0200

    Update build files to match zig 0.12

--- build.zig
+++ build.zig
@@ -1,42 +1,46 @@
 const std = @import("std");
-const rl = @import("raylib-zig/build.zig");
+const rlz = @import("raylib-zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    var raylib = rl.getModule(b, "raylib-zig");
-    var raylib_math = rl.math.getModule(b, "raylib-zig");
+    
+    const raylib_dep = b.dependency("raylib-zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const raylib = raylib_dep.module("raylib");
+    const raylib_math = raylib_dep.module("raylib-math");
+    const raylib_artifact = raylib_dep.artifact("raylib");
+
     //web exports are completely separate
-    if (target.getOsTag() == .emscripten) {
-        const exe_lib = rl.compileForEmscripten(b, "Crimsonlink", "src/main.zig", target, optimize);
-        exe_lib.addModule("raylib", raylib);
-        exe_lib.addModule("raylib-math", raylib_math);
-        const raylib_artifact = rl.getArtifact(b, target, optimize);
-        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
+    if (target.query.os_tag == .emscripten) {
+        const exe_lib = rlz.emcc.compileForEmscripten(b, "Crimsonlink", "src/main.zig", target, optimize);
+
         exe_lib.linkLibrary(raylib_artifact);
-        const link_step = try rl.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
+        exe_lib.root_module.addImport("raylib", raylib);
+        exe_lib.root_module.addImport("raylib-math", raylib_math);
+
+        // Note that raylib itself is not actually added to the exe_lib output file, so it also needs to be linked with emscripten.
+        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
+
         b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rl.emscriptenRunStep(b);
+        const run_step = try rlz.emcc.emscriptenRunStep(b);
         run_step.step.dependOn(&link_step.step);
-        const run_option = b.step("run", "Run Project");
+        const run_option = b.step("run", "Run Crimsonlink");
         run_option.dependOn(&run_step.step);
         return;
     }
 
     const exe = b.addExecutable(.{ .name = "Crimsonlink", .root_source_file = .{ .path = "src/main.zig" }, .optimize = optimize, .target = target });
 
-    const test_step = b.step("test", "Run tests");
-
-    const main_tests = b.addTest(.{ .name = "test", .root_source_file = .{ .path = "src/main.zig"} });
-    const run_main_test = b.addRunArtifact(main_tests);
-    test_step.dependOn(&run_main_test.step);
-
-    rl.link(b, exe, target, optimize);
-    exe.addModule("raylib", raylib);
-    exe.addModule("raylib-math", raylib_math);
+    exe.linkLibrary(raylib_artifact);
+    exe.root_module.addImport("raylib", raylib);
+    exe.root_module.addImport("raylib-math", raylib_math);
 
     const run_cmd = b.addRunArtifact(exe);
-    const run_step = b.step("run", "Run Project");
+    const run_step = b.step("run", "Run Crimsonlink");
     run_step.dependOn(&run_cmd.step);
 
     b.installArtifact(exe);
